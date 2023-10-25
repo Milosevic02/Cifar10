7791d4ceb820c0efb0a3ee1754cbd019f659d754
Dragan <milosevicdragan002@gmail.com>
2023-10-25 15:00:02 +0200

    include all libary

---
+++ main.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.layers import Input,Conv2D,Dense,Flatten,Dropout,GlobalMaxPooling2D,MaxPooling2D,BatchNormalization
+from keras.models import Model
