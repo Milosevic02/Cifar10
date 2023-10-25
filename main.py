f380284d23b668c657fb2d4a0f32f26817a8a5d2
Dragan <milosevicdragan002@gmail.com>
2023-10-25 15:09:19 +0200

    Create model

--- main.py
+++ main.py
@@ -10,3 +10,38 @@ x_train,x_test = x_train/255.0 , x_test /255.0
 y_train,y_test = y_train.flatten(),y_test.flatten()
 print("x_train.shape: " , x_train.shape)
 print("y_train.shape",y_train.shape)
+
+K = len(set(y_train))
+print("number of classes: ",K)
+
+i = Input(shape=x_train[0].shape)
+
+x = Conv2D(32,(3,3),activation='relu',padding='same')(i)
+x = BatchNormalization()(x)
+x = Conv2D(32,(3,3),activation='relu',padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2,2))(x)
+x = Dropout(0.2)(x)
+
+x = Conv2D(64,(3,3),activation='relu',padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(64,(3,3),activation='relu',padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2,2))(x)
+x = Dropout(0.2)(x)
+
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2, 2))(x)
+x = Dropout(0.2)(x)
+
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(1024,activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(K,activation='softmax')(x)
+
+model = Model(i,x)
+
