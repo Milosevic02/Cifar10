04f2b22bc332f2b1ac4a0c86738f7135e56fd4f4
Dragan <milosevicdragan002@gmail.com>
2023-10-25 15:11:43 +0200

    model fit

--- main.py
+++ main.py
@@ -46,3 +46,10 @@ x = Dense(K,activation='softmax')(x)
 model = Model(i,x)
 
 model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+batch_size = 32
+data_generator = tf.keras.preprocessing.image.ImageDataGenerator(width_shift_range=0.1,height_shift_range=0.1,horizontal_flip=True)
+train_generator = data_generator.flow(x_train,y_train,batch_size)
+steps_per_epoch = x_train.shape[0] // batch_size
+
+r = model.fit(train_generator, validation_data=(x_test, y_test), steps_per_epoch=steps_per_epoch, epochs=50)
